b686fd42b6e1f2998b14444ba7bb7d309cff4c72
Jordan Andrigo <jordanandrigo@Jordans-MacBook-Pro.local>
2020-12-06 00:23:57 -0500

    updating html files

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ import { SendMail } from "./components/mailer.js";
 
         // show some UI here to let the user know the mail attempt was successful
     }
+
     
     function processMail(event) {
         // block the default submit behaviour
